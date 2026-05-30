297dedc9fc636c2a9e2e0f942b71073eb904b7c0
n-h-a <hanikki811@gmail.com>
2026-05-30 14:14:06 -0700

    Updated requirements + set up the database connection infrastructure (engine, session factor, Base class)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+
+from app.database import get_db
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -15,17 +18,24 @@ def root():
     return {"message": "Welcome."}
 
 @app.post("/telemetry")
-def process_readings(t : Telemetry):
+def process_readings(t : Telemetry, db : Session = Depends(get_db)):
+    # Accept telemetry from satellites
+    # Validate it
+    # Store it in database
+    # Create alert if needed
     ...
 
 @app.get("/telemetry/recent")
-def get_readings():
+def get_readings(db : Session = Depends(get_db)):
+    # List recent readings
     ...
 
 @app.get("/sources")
-def get_sources():
+def get_sources(db : Session = Depends(get_db)):
+    # List satellites or sensors
     ...
 
 @app.get("/alerts")
-def get_alerts():
+def get_alerts(db : Session = Depends(get_db)):
+    # List generated alerts
     ...
